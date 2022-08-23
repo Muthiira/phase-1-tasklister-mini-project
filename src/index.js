1a5f1e3d5e3df9746b528ece5cdb863ecf8646d7
Muthiira <calebmuchiri48@gmail.com>
2022-08-23 13:31:32 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+ //console.log('After dom loaded')
+ document.querySelector('form').addEventListener('submit',(e)=>{
+  e.preventDefault()
+  handleToDo(e.target.new_task.value)
+ })
+})
+
+//the task added by the user should appear in the dom..append
+function handleToDo(todo){
+  let li = document.createElement('li')
+  li.textContent = todo
+  document.querySelector("#tasks").appendChild(li)
+  //console.log(p)
+}
